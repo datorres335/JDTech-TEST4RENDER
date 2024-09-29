95582b2c69da3e2869133485601d84a03c751920
Jihnboii <rodriguezjohn910@gmail.com>
2024-09-29 00:01:37 -0700

    Add navigation partial and update application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#home" # The home controller and home action for the root URL
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Example resource routes (controllers and actions should exist)
+  get 'products', to: 'products#index'
+  get 'cart', to: 'cart#show'
+  get 'login', to: 'sessions#new'
 end
