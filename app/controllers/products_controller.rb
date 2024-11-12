d3b1d2026a6e15e83477f98b891c80efad71426b
David Torres <davidatorres@cpp.edu>
2024-11-11 22:51:52 -0800

    search bar added, not working tho

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    # # TO DISPLAY PRODUCTS FROM SEARCH BAR  # CODE NOT WORKING AS OF NOW
+    # if params[:search].present?
+    #   @products = Product.where("name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    # else
+    #   @products = Product.all
+    # end
   end
 end
