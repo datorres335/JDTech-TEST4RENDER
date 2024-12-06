12501e9dd63d429238b96aa4df10e9b2f2b951a3
David Torres <davidatorres@cpp.edu>
2024-12-05 22:43:37 -0800

    deleted uncessary migration files

--- db/migrate/20241201025642_remove_user_id_index_from_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdIndexFromProducts < ActiveRecord::Migration[7.2]
-  def change
-    remove_index :products, column: :user_id, name: "index_products_on_user_id"
-  end
-end
--- db/migrate/20241201030418_drop_user_id_index_from_products.rb
+++
@@ -1,5 +0,0 @@
-class DropUserIdIndexFromProducts < ActiveRecord::Migration[7.2]
-  def change
-    remove_index :products, name: "index_products_on_user_id"
-  end
-end
