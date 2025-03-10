51a6977e49e8446a93031a03e1c494e9f0288bd1
datorres335 <datorres335@gmail.com>
2025-03-09 20:24:33 -0700

    edited assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,5 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
-Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.otf )
+#Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.ttf *.otf )
 
