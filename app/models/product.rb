20c38cb29c510b47409bc192e0768f603b10d9e2
datorres335 <datorres335@gmail.com>
2025-03-12 22:44:54 -0700

    added sections to the products page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   #Cart items
   has_many :cart_items
   # Favorites 
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorited_by_users, through: :favorites, source: :user
 end
